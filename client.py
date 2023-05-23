a16f91420d320620947f3819879b1b53fdab61a2
Marc LLOP <marc.llop@linkso.fr>
2023-05-23 15:15:24 +0200

    Ajout d'un prototype card avec son implementation en python

--- client.py
+++ client.py
@@ -18,8 +18,9 @@ from __future__ import print_function
 import logging
 
 import grpc
-import helloworld_pb2 as pb2
-import helloworld_pb2_grpc as pb2_grpc
+
+import card_pb2 as pb2
+import card_pb2_grpc as pb2_grpc
 
 
 def run():
@@ -27,10 +28,14 @@ def run():
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     print("Will try to greet world ...")
+
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = pb2_grpc.GreeterStub(channel)
-        response = stub.SayHello(pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
+        stub = pb2_grpc.CardStub(channel)
+        ids: pb2.IdCardsResponse = stub.getCards(pb2.Empty())
+
+        for i in ids.result:
+            card: pb2.FullCard = stub.getFullCard(pb2.SimpleCard(id=i))
+            print(card)
 
 
 if __name__ == '__main__':
